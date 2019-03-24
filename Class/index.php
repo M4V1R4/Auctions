ea8cca2605c4b9daca5f16c94b6a41ff825283a5
Nelson <nmoraj@est.utn.ac.cr>
2019-03-24 01:35:14 -0600

    changes in class form.php

--- Class/index.php
+++ Class/index.php
@@ -18,7 +18,6 @@ $producto = ['description' => $description, 'id_picture' => $id_picture, 'produc
 	
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
 	require_once '../shared/db.php';
-	require 'save_task';
 	$product_model->create($descripcion, $id_picture, $id_admin);
 	return header('Location: ../index.php');
 }
@@ -29,38 +28,9 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     <?php require_once 'form.php' ?>
 </div>
  
-	<script type="text/javascript">
-		var numero = 0; 
-		evento = function (evt) { 
-		   return (!evt) ? event : evt;
-		}
-		addCampo = function () { 
-		   nDiv = document.createElement('div');
-		   nDiv.className = 'archivo';
-		   nDiv.id = 'file' + (++numero);
-		   nCampo = document.createElement('input');
-		   nCampo.name = 'archivos[]';
-		   nCampo.type = 'file';
-		   a = document.createElement('a');
-		   a.name = nDiv.id;
-		   a.href = '#';
-		   a.onclick = elimCamp;
-		   a.innerHTML = 'Eliminar';
-		   nDiv.appendChild(nCampo);
-		   nDiv.appendChild(a);
-		   container = document.getElementById('adjuntos');
-		   container.appendChild(nDiv);
-		}
-		elimCamp = function (evt){
-		   evt = evento(evt);
-		   nCampo = rObj(evt);
-		   div = document.getElementById(nCampo.name);
-		   div.parentNode.removeChild(div);
-		}
-		rObj = function (evt) { 
-		   return evt.srcElement ?  evt.srcElement : evt.target;
-		}
-	</script> 					
+<div class="container">
+    <?php require_once 'javascript.php' ?>
+</div>					
 
 
 <!-- include("fooder.php") -->
