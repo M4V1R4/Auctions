fc58b32f414314bb68b378e30202756c4393116d
Nelson <nmoraj@est.utn.ac.cr>
2019-03-23 21:06:12 -0600

    add class product and modified index.php

--- Class/index.php
+++ Class/index.php
@@ -1,14 +1,27 @@
 <?php include("header.php")?>
+<?php
+	$title = 'Registro de productos';
+
+	$descripcion = $_POST['descripcion'];
+	$id_picture = $_POST['id_picture'];
+	$id_admin = $_POST['id_admin'];
+
+	$producto = ['descripcion' => $descripcion, 'id_picture' => $id_picture, 'id_admin' => $id_admin];
+	
+
+?>
 
 <div class="container p-4">
     <div class="row">
         <div class="col-md-6" >
                 <form action="saving_task.php">
                     <div class="form-group">
+					<h1>Nombre del producto:</h1>
                         <input type="text" name="title" class="form-control" 
                         placeholder="Nombre del producto" autofocus>
                     </div>
                     <div class="form-group">
+					<h1>Descripcion:</h1>
                         <textarea name="description" rows="2" class="form-control"
                         placeholder="Detalles del producto"></textarea>
                     </div>
@@ -58,7 +71,8 @@
 	</script> 
                     <input type="submit" class="btn btn-success btn-block" name= "save_task"
                     value="Guardar producto">
-
+					<input type="submit" class="btn btn-danger btn-block" name= "Cancel"
+                    value="Cancelar">
                 </form>
             </div>
 
---
+++ Models/Product.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Models {
+
+    class Product
+    {
+        private $connection;
+        public function __construct($connection)
+        {
+            $this->connection = $connection;
+        }
+
+        public function find($id)
+        {
+            return $this->connection->runQuery('select * from users where id = $1', [$id])[0];
+        }
+        public function create($descripcion, $id_picture, $id_admin)
+        {
+            $this->connection->runStatement('INSERT INTO users (descripcion, id_picture, id_admin) VALUES ($1, $2, $3)', [$descripcion, $id_picture, $id_admin]);
+        }
+    }
+
+}
