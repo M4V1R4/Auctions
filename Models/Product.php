f5e08b586a16db9a1694c970e292b050b6cae45d
Nelson <nmoraj@est.utn.ac.cr>
2019-03-26 23:40:50 -0600

    Class modified product and add class products

--- Models/Product.php
+++ Models/Product.php
@@ -15,7 +15,20 @@ namespace Models {
             $this->connection->runStatement('INSERT INTO products (nombre, description, id_admin) 
             VALUES ($1, $2, $3)', [$nombre, $description, $id_admin]);
         }
-    }
-                          
 
+        public function delete($id)
+        {
+            $this->connection->runStatement('DELETE FROM products WHERE id = $1', [$id]);
+        }
+
+        public function update($id, $brand, $year)
+        {
+            $this->connection->runStatement('UPDATE cars SET brand = $2, year = $3 WHERE id = $1', [$id, $brand, $year]);
+        }
+
+        public function all()
+        {
+            return $this->connection->runQuery('SELECT * FROM products');
+        }
+    }                 
 }
